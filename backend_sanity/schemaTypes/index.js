cf5edd8dbd9f3f7331b39bb59b2fe6f43280e2ea
Velid Posko <velid.posko@zira.com.ba>
2024-03-02 09:32:14 +0100

    Backend folder added

---
+++ backend_sanity/schemaTypes/index.js
@@ -0,0 +1,12 @@
+import testimonials from "./testimonials";
+import about from "./about";
+import brands from "./brands";
+import contact from "./contact";
+import experiences from "./experiences";
+import skills from "./skills";
+import workExperience from "./workExperience";
+import works from "./works";
+
+
+
+export const schemaTypes = [testimonials, about, brands, contact, experiences, skills, workExperience, works]
