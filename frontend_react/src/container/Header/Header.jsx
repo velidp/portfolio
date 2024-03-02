c456fa076564cd740471ca2eda8db9ce5841ce26
Velid Posko <velid.posko@zira.com.ba>
2024-03-02 14:28:38 +0100

    Header implemented

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,10 +1,75 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 
 import './Header.scss';
 
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <div id='home' className='app__header app__flex'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👾</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>ٱلسَّلَامُ عَلَيْكُمْ</p>
+              <p className='p-text'>I am</p>
+              <h1 className='head-text'>Velid Poško</h1>
+            </div>
+          </div>
+          <div className='tag-cmp app__flex'>
+            <p className='p-text'>Software Engeener</p>
+            <p className='p-text'>Full Stack Wizard</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profile} alt='profile_bg'></img>
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          className='overlay_circle'
+          src={images.circle}
+          alt='profile_circle'
+        />
+      </motion.div>
+
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className='app__header-circles'
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className='circle-cmp app_flex' key={`circle-${index}`}>
+            <img src={circle} alt='circle'/>
+          </div>
+        ))}
+      </motion.div>
+
+    </div>
   )
 }
 
