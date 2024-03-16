933bc93e217aa6376e856d61062fed98f25f2b95
Velid Posko <velid.posko@zira.com.ba>
2024-03-16 21:26:23 +0100

    Motion Wrapper Added

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { Tooltip } from 'react-tooltip';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Skills.scss';
 
@@ -90,4 +90,11 @@ const Skills = () => {
   );
 }
 
-export default AppWrap(Skills, 'skills');
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'), 
+  'skills',
+  'app__whitebg'
+);
+
+
+// export default AppWrap(Skills, 'skills');
