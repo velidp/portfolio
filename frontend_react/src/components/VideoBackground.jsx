e6c11494ac398e351f86a257632b7c9480f469b2
Velid Posko <velid.posko@zira.com.ba>
2024-07-11 14:04:33 +0200

    Video Background Fixed

--- frontend_react/src/components/VideoBackground.jsx
+++ frontend_react/src/components/VideoBackground.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Header } from '../container';
 import './VideoBackground.scss'; 
+import YouTube from 'react-youtube';
 
 const VideoBackground = () => {
+
+  const opts = {
+    height: '100%',
+    width: '100%',
+    playerVars: {
+      autoplay: 1,
+      mute: 1,
+      loop: 1,
+      playlist: "HIvqw74pCFc",
+      controls: 0, 
+    },
+  };
+
   return (
     <div className="video-background">
-      <video autoPlay muted loop>
-      <source src="/video_background_2k.mp4" type="video/mp4" />
-
-        {/* Add additional <source> elements for different video formats */}
-        Your browser does not support the video tag.
-      </video>
-      {/* Add content on top of the video background */}
-      <div >
-        <Header/>
+      <div className="video">
+        <YouTube videoId={"HIvqw74pCFc"} opts={opts} className="video-iframe" />
+        <div className="overlay"></div>
+        <div className="header-container">
+          <Header/>
+        </div>
       </div>
     </div>
   );
