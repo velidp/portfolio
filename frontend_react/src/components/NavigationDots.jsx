eeb52e7a07c4dcb6e5059e79396a79e48985f1b9
Velid Posko <velid.posko@zira.com.ba>
2024-07-15 20:08:04 +0200

    One more little fix

--- frontend_react/src/components/NavigationDots.jsx
+++ frontend_react/src/components/NavigationDots.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
-    {['home', 'about', 'work', 'skill', 'testimonials', 'contact'].map((item, index) => (
+    {['home', 'about', 'work', 'experience', 'testimonials', 'contact'].map((item, index) => (
       <a
         href={`#${item}`}
         key={item + index}
