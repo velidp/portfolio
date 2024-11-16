cef3302de7c480f88096edc62c98cd5c34c8c5e2
Velid Posko <veld.posko@zira.com.ba>
2024-11-16 13:08:29 +0100

    Four items per page fix

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { HiChevronLeft, HiChevronDoubleRight, HiChevronRight } from 'react-icons/hi';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
-
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 import { MotionWrap, AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
@@ -13,6 +13,7 @@ const Work = () => {
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const query = '*[_type == "works"]';
@@ -22,6 +23,15 @@ const Work = () => {
       setFilterWork(data);
     })
   }, [])
+
+
+  const handleClick = (index) => {
+    if(index < filterWork.length) {
+      setCurrentIndex(index);
+    } else {
+      setCurrentIndex(0);
+    }
+  }
   
 
   const handleWorkFilter = (item) => {
@@ -57,7 +67,8 @@ const Work = () => {
           </div>
         ))}
       </div>
-
+      
+      {filterWork.length < 4 ? 
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
@@ -109,7 +120,75 @@ const Work = () => {
 
           </div>
         ) )}
-      </motion.div>
+      </motion.div> 
+      :
+
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__work-portfolio'
+      >
+        {filterWork.slice(currentIndex, currentIndex + 4).map((work, index) => (
+          <div className='app__work-item app__flex' key={index}>
+            <div className='app__work-img app__flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5}}
+                className='app__work-hover app__flex'
+              >
+                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9]}}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'
+                  >
+                    <AiFillEye/>
+                  </motion.div>
+                </a>
+
+                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9]}}
+                    transition={{ duration: 0.25 }}
+                    className='app__flex'
+                  >
+                    <AiFillGithub/>
+                  </motion.div>
+                </a>
+
+              </motion.div>
+            </div>
+
+            <div className='app__work-content app__flex'>
+              <h4 className='title-text'>{work.title}</h4>
+              <p className='desription-text'>{work.description}</p>
+
+              <div className='app__work-tag app__flex'>
+                <p>{work.tags[0]}</p>
+              </div>
+            </div>
+
+          </div>
+        ) )}
+      
+
+        <div className='app__testimonial-btns app__flex'>
+            <div className='app__flex' onClick={() => handleClick(currentIndex === 0 ? Math.floor(works.length / 4) * 4 : currentIndex - 4)}> 
+              <HiChevronLeft/>
+            </div>
+            <div className='app__flex' onClick={() => handleClick(currentIndex + 4)}> 
+              <HiChevronRight/>
+            </div>
+          </div>
+      </motion.div> 
+
+
+      
+      } 
     </>
   )
 }
