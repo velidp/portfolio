ef9abf673870be292c72b4598a191248012a500c
Velid Posko <veld.posko@zira.com.ba>
2025-02-12 20:22:32 +0100

    More fixes

--- frontend_react/src/components/VideoBackground.jsx
+++ frontend_react/src/components/VideoBackground.jsx
@@ -1,44 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { Header } from '../container';
-import './VideoBackground.scss'; 
-import YouTube from 'react-youtube';
-import { client } from '../../src/client';
+import React, { useState, useEffect } from "react";
+import { Header } from "../container";
+import "./VideoBackground.scss";
+import YouTube from "react-youtube";
+import { client } from "../../src/client";
 
 const VideoBackground = () => {
-
   const [home, setHome] = useState([]);
 
-    useEffect(() => {
-        const query = '*[_type == "home"]';
-        client.fetch(query)
-            .then((data) => setHome(data))
-    }, []);
+  useEffect(() => {
+    const query = '*[_type == "home"]';
+    client.fetch(query).then((data) => setHome(data));
+  }, []);
 
   const opts = {
-    height: '100%',
-    width: '100%',
+    height: "100%",
+    width: "100%",
     playerVars: {
       autoplay: 1,
       mute: 1,
-      loop: 1,
-      playlist: home[0]?.videoId,
-      controls: 0
+      controls: 0,
     },
+  };
+
+  const onEnd = (event) => {
+    event.target.seekTo(0);
   };
 
   return (
     <div className="video-background">
       <div className="video">
         <div className="video-container">
-          <YouTube videoId={"np7CvKfxjE4"} opts={opts} className="video-iframe" />
+          {home[0]?.videoId && (
+            <YouTube
+              videoId={home[0].videoId}
+              opts={opts}
+              className="video-iframe"
+              onEnd={onEnd} 
+            />
+          )}
         </div>
         <div className="overlay"></div>
         <div className="header-container">
-          <Header/>
+          <Header />
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default VideoBackground;
